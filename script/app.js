3b0659a5b3ea29f45d0694516af11ab0b36eca0e
William Wu <williamwuu3@gmail.com>
2023-12-08 11:23:12 -0800

    fixed toggle menu

--- script/app.js
+++ script/app.js
@@ -1,9 +1,11 @@
 const toggleEl = document.querySelector('.toggle');
-
+const navMenu = document.getElementById('nav_menu');
 
 toggleEl.addEventListener('click', () => {
 	console.log('click');
     const isExpanded = toggleEl.getAttribute('aria-expanded') === 'true';
     // Toggle the aria-expanded attribute
     toggleEl.setAttribute('aria-expanded', String(!isExpanded));
+		// Toggle the class on the #nav_menu element
+		navMenu.classList.toggle('expanded', !isExpanded);
 });
